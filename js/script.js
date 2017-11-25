dffe96eb5435e9e4e8842fe374207d8012990899
david-binda <david.binovec@gmail.com>
2017-11-25 23:35:53 +0100

    Adding article part on class selectors and extending the listeners to class (.css-rules) instead of #css
    This commit also changes .walls class to .wall as it is more accurate.

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@
 		canvas : document.querySelector( '#canvas' ),
 		processChange: function( event ) {
 			var value = event.target.value;
+			console.log( event );
 			if ( ( value.startsWith( '.' ) || value.startsWith( '#' ) ) && value.length > 1 ) {
 				this.removePreviousHighlight();
 				this.highlight( value );
@@ -28,10 +29,17 @@
 			}
 		},
 		init: function() {
-			var that = this;
-			document.querySelector('#css').onchange = function( event ) { that.processChange( event ); };
-			document.querySelector('#css').oninput = function( event ) { that.processChange( event ); };
-			document.querySelector('#css').onpaste = function( event ) { that.processChange( event ); };
+			var that = this,
+			    elements = document.querySelectorAll('.css-rules');
+
+			if ( 0 !== elements.length ) {
+				for ( var i = 0; i < elements.length; i++ ) {
+					elements[i].addEventListener( 'change', function( event ) { that.processChange( event ); } );
+					elements[i].addEventListener( 'input', function( event ) { that.processChange( event ); } );
+					elements[i].addEventListener( 'paste', function( event ) { that.processChange( event ); } );
+				}
+			}
+				
 		}
 	}
 	document.addEventListener( 'DOMContentLoaded', function(event) {
