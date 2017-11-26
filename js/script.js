c6d9bf300b299d6aee476bb0ef4c27681a39e384
david-binda <david.binovec@gmail.com>
2017-11-26 11:55:27 +0100

    Adding first exercise and exercise evaluation basics to the JS
    This commit adds foundations for creating an exercise for the explanation text. The first exercise on basic class selectors consists of selecting various parts of the house.

--- js/script.js
+++ js/script.js
@@ -3,13 +3,39 @@
 		canvas : document.querySelector( '#canvas' ),
 		processChange: function( event ) {
 			var value = event.target.value;
-			console.log( event );
+			// Highlight the elements, if there are some.
 			if ( ( value.startsWith( '.' ) || value.startsWith( '#' ) ) && value.length > 1 ) {
 				this.removePreviousHighlight();
 				this.highlight( value );
 			} else {
 				this.removePreviousHighlight();
 			}
+			// If selecting in terms of an exercise, evaluate the output.
+			var exercise = event.target.getAttribute( 'data-exercise-form-for' );
+			if ( null !== exercise ) {
+				var tasks = document.querySelectorAll( '#' + exercise + ' .task' );
+				if ( 0 !== tasks.length ) {
+					for ( var i = 0; i < tasks.length; i++ ) {
+						var task = tasks[i],
+						    taskType = task.getAttribute( 'data-exercise-type' ),
+						    failed = false;
+						if ( 'select' === taskType ) {
+							var expectedSolution = this.canvas.querySelectorAll( task.getAttribute( 'data-exercise-solution' ) );
+							if ( 0 !== expectedSolution.length ) {
+								for ( var j = 0; j < expectedSolution.length; j++ ) {
+									if ( ! expectedSolution[j].classList.contains( 'highlight' ) ) {
+										failed = true;
+									}
+								}
+								if ( false === failed ) {
+									task.classList.add( 'success' );
+								}
+							}
+						}
+					}
+				}
+			}
+			
 		},
 		highlight: function( selector ) {
 			var elements = this.canvas.querySelectorAll( selector );
